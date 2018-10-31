dcc8e8dcf4d9304e4915e98d326e084646d91866
VigneshwarJ <36656071+VigneshwarJ@users.noreply.github.com>
2018-10-31 16:20:58 +0530

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -8,12 +8,15 @@ using namespace std;
 #include <stdio.h>
 #include <Windows.h>
 
-int nScreenWidth = 180;			// Console Screen Size X (columns)
-int nScreenHeight = 130;			// Console Screen Size Y (rows)
+int nScreenWidth = 100;			// Console Screen Size X (columns)
+int nScreenHeight = 30;		// Console Screen Size Y (rows)
+int countt = 0;
+class Sort;
+void print(Sort* sort, wchar_t* screen, HANDLE& hConsole, DWORD& dwBytesWritten);
 class Sort
 {
 private:
-	
+
 	int temp;
 	int count;
 	int no;
@@ -24,7 +27,7 @@ public:
 	{
 		for (int i = no - 1; i >= 0; i--)
 		{
-			x[i] = 99 - i;
+			x[i] = no-1-i;
 
 
 		}
@@ -36,14 +39,14 @@ public:
 	{
 		for (int i = no - 1; i >= 0; i--)
 		{
-			x[i] = no -1 - i;
+			x[i] = no - 1 - i;
 
 
 		}
 	}
 	void random()
 	{
-		for (int i = 0; i<no; i++)
+		for (int i = 0; i < no; i++)
 		{
 
 			int j = rand() % no;
@@ -53,7 +56,7 @@ public:
 
 		}
 	}
-	void BubbleSort()
+	void BubbleSort(wchar_t* screen, HANDLE& hConsole, DWORD& dwBytesWritten)
 	{
 		count = 0;
 		bool sorted = false;
@@ -62,7 +65,7 @@ public:
 		{
 
 			sorted = true;
-			for (int j = 0; j<last; j++)
+			for (int j = 0; j < last; j++)
 			{
 
 				if (x[j + 1] < x[j])
@@ -72,13 +75,15 @@ public:
 					x[j + 1] = temp;
 					sorted = false;
 				}
-
+				::print(this, screen, hConsole, dwBytesWritten);
 				count++;
 			}
 			last--;
+			
 			//this->print();
 
 		}
+		
 	}
 	void insertionSort()
 	{
@@ -86,7 +91,7 @@ public:
 		for (int i = 1; i < no; i++)
 		{
 			int k = i;
-			for (int j = i - 1; j>-1; j--)
+			for (int j = i - 1; j > -1; j--)
 			{
 				count++;
 				//this->print();
@@ -121,101 +126,67 @@ public:
 		a = b;
 		b = temp;
 	}
-	//void print()
-	//{
-	//	for (int i = 0; i<no; i++)
-	//	{
-	//		std::cout << x[i] << "	";
-	//		if ((i + 1) % 10 == 0)
-	//		{
-	//			std::cout << std::endl;
-	//		}
-
-
-	//	}
-	//	if (x[no - 1] == no - 1)
-	//	{
-	//		std::cout << "Sorted" << std::endl;
-	//	}
-	//	else
-	//		std::cout << "not Sorted" << std::endl;
-	//}
-	//void printcount()
-	//{
-	//	std::cout << "count =" << count << std::endl;
-	//}
-
+	
 };
 
 
 int main()
 {
-	void print(Sort sort, wchar_t* screen);
-	// Create Screen Buffer
-	/*wchar_t *screen = new wchar_t[nScreenWidth*nScreenHeight];*/
-	HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
-	_CONSOLE_SCREEN_BUFFER_INFO info;
 	
+	// Create Screen Buffer
+	HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+
+	_CONSOLE_SCREEN_BUFFER_INFO info;
+
 	GetConsoleScreenBufferInfo(hConsole, &info);
-	if (info.dwSize.X<nScreenWidth )
+
+	if (info.dwSize.X < nScreenWidth)
 	{
 		nScreenWidth = info.dwSize.X;
 	}
-	if (info.dwSize.Y<nScreenHeight)
+	if (info.dwSize.Y < nScreenHeight)
 	{
 		nScreenHeight = info.dwSize.Y;
 	}
+
 	wchar_t *screen = new wchar_t[nScreenWidth*nScreenHeight];
+
 	COORD coord = { nScreenWidth ,nScreenHeight };
 	SetConsoleScreenBufferSize(hConsole, coord);
 
-	SMALL_RECT windowSize = { 0, 0, nScreenWidth-1, nScreenHeight-1 };
+	SMALL_RECT windowSize = { 0, 0, nScreenWidth - 1, nScreenHeight - 1 };
 	SetConsoleWindowInfo(hConsole, TRUE, &windowSize);
+	
 	if (!SetConsoleScreenBufferSize(hConsole, coord))
 	{
 		cout << "SetConsoleWindowInfo failed with error " << GetLastError() << endl;
 		return -1;
 	}
 	SetConsoleActiveScreenBuffer(hConsole);
-	
+
 	DWORD dwBytesWritten = 0;
 
 	Sort sort(nScreenHeight);
 	auto tp1 = chrono::system_clock::now();
 	auto tp2 = chrono::system_clock::now();
 
-	while (1)
+	while (!(GetAsyncKeyState((unsigned short)'A') & 0x8000))
 	{
 		tp2 = chrono::system_clock::now();
 		chrono::duration<float> elapsedTime = tp2 - tp1;
 		tp1 = tp2;
 		float fElapsedTime = elapsedTime.count();
+		::print(&sort, screen,hConsole,dwBytesWritten);
+		sort.BubbleSort(screen,hConsole,dwBytesWritten);
 
-		//for (int x = 0; x < nScreenWidth; x++)
-		//{
-		//				
-		//				for (int y = 0; y < nScreenHeight; y++)
-		//				{
-		//					// Each Row
-		//					if (x	 < sort.x[y])
-		//						screen[y*nScreenWidth + x] = '_';
-		//					else 
-		//						screen[y*nScreenWidth + x] = ' ';
-		//					
-		//				}
-		//			}
-		print(sort, screen);
-		sort.BubbleSort();
-				
-					// Display Frame
-					screen[nScreenWidth * nScreenHeight - 1] = '\0';
-					WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth * nScreenHeight, { 0,0 }, &dwBytesWritten);
-				}
+	
+		
+	}
 
-				return 0;
+	return 0;
 }
 
-void print(Sort sort,wchar_t* screen)
+void print(Sort* sort, wchar_t *screen,HANDLE& hConsole,DWORD& dwBytesWritten)
 {
 	for (int x = 0; x < nScreenWidth; x++)
 	{
@@ -223,14 +194,19 @@ void print(Sort sort,wchar_t* screen)
 		for (int y = 0; y < nScreenHeight; y++)
 		{
 			// Each Row
-			if (x	 < sort.x[y])
+			if (x < sort->x[y])
+			{
 				screen[y*nScreenWidth + x] = '_';
+			}
 			else
 				screen[y*nScreenWidth + x] = ' ';
-
+	
 		}
 	}
+	Sleep(50);
+	screen[nScreenWidth * nScreenHeight - 1] = '\0';
+	WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth * nScreenHeight, { 0,0 }, &dwBytesWritten);	// Display Frame
 
+	
+	
 }
-
-// That's It!! - Jx9
